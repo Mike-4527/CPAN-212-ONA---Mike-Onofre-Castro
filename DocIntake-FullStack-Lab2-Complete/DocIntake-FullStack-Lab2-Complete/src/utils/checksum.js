aa1d46e912256ef6189c5e34308d8c2370cb2afe
Mike-4527 <n01619920@humber.ca>
2026-02-13 19:56:25 -0500

    Delete DocIntake-FullStack-Lab2-Complete/DocIntake-FullStack-Lab2-Complete directory

--- DocIntake-FullStack-Lab2-Complete/DocIntake-FullStack-Lab2-Complete/src/utils/checksum.js
+++
@@ -1,5 +0,0 @@
-import crypto from "crypto";
-
-export function sha256Hex(text) {
-  return crypto.createHash("sha256").update(text, "utf8").digest("hex");
-}
--- DocIntake-FullStack-Lab2-Complete/DocIntake-FullStack-Lab2-Complete/src/utils/id.js
+++
@@ -1,5 +0,0 @@
-import crypto from "crypto";
-
-export function newId() {
-  return crypto.randomUUID();
-}
